6f0528088941bd3c00a36a5998862204ec7e34cb
zerohzz <alexander.zzzz@hotmail.com>
2017-09-11 16:58:46 +1000

    Add files via upload

---
+++ Sitting.py
@@ -0,0 +1,31 @@
+# This program is on Python 2.7
+# Answering the "Sitting or Standing?" problem
+# http://orac.amt.edu.au/cgi-bin/train/problem.pl?set=simple1&problemid=342
+
+import sys
+
+#Open input files
+infile = open("sitin.txt","r")
+
+#Open output files
+outfile = open("sitout.txt","w")
+
+#Read the input into the string, split by space
+string1 = infile.read().split()
+
+
+#Read numbers
+row = int(string1[0])
+seat = int(string1[1])
+ticket = int(string1[2])
+
+#The output solution
+solution1 = row * seat
+solution2 = ticket - row*seat
+
+#write in the file
+outfile.write(str(solution1)+" " + str(solution2))
+
+# Finally, close the input/output files.
+infile.close()
+outfile.close()
