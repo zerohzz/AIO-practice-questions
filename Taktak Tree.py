20e306ed6772f222b9b4bb7061d0165e0604ccad
zerohzz <alexander.zzzz@hotmail.com>
2017-09-11 17:27:05 +1000

    Update Taktak Tree.py

--- Taktak Tree.py
+++ Taktak Tree.py
@@ -2,8 +2,6 @@
 # Answering the "The Tremendous Tak-Tak Tree" problem
 # http://orac.amt.edu.au/cgi-bin/train/problem.pl?set=simple2&problemid=382
 
-import sys
-
 #Open input files
 infile = open("taktakin.txt","r")
 
