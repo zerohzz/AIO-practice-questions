97ad462edaa4ad458a624a937d75a407c9b7d5d6
zerohzz <alexander.zzzz@hotmail.com>
2017-09-11 17:18:50 +1000

    Add files via upload

---
+++ Taktak Tree.py
@@ -0,0 +1,30 @@
+
+import sys
+
+#Open input files
+infile = open("taktakin.txt","r")
+
+#Open output files
+outfile = open("taktakout.txt","w")
+
+#Read the input into the string, split by space
+string1 = infile.read().split()
+print string1
+
+days = 0
+fruit = int(string1[0])
+
+
+while (fruit % 11 != 1):
+    fruit = fruit * 2
+    days = days + 1
+
+
+#The output solution
+solution = str(days) + " " + str(fruit)
+#write in the file
+outfile.write(str(solution))
+
+# Finally, close the input/output files.
+infile.close()
+outfile.close()
